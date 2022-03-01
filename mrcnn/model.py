dc388627b2a45ad23fd5a5bf0445c2f129245e65
rising1 <phfrost@ymail.com>
2022-03-01 20:58:27 +0000

    1st attempt to run train on example balloon

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -2276,7 +2276,8 @@ class MaskRCNN():
         # Directory for training logs
 #        self.log_dir = os.path.join(self.model_dir, "{}{:%Y%m%dT%H%M}".format(
 #            self.config.NAME.lower(), now))
-        self.log_dir = "//logdir//train"
+        #self.log_dir = "//logdir//train"
+        self.log_dir = "C:/Users/phfro/PycharmProjects/Mask-RCNN/logs"
 
         # Path to save after each epoch. Include placeholders that get filled by Keras.
         self.checkpoint_path = os.path.join(self.log_dir, "mask_rcnn_{}_*epoch*.h5".format(
