f7d50933731da313665996f3b9960ca01816b766
rising1 <phfrost@ymail.com>
2022-03-16 20:48:29 +0000

    looking at the dataset and annotations

--- fashion-transfer-learning/json_test.py
+++ fashion-transfer-learning/json_test.py
@@ -6,8 +6,8 @@ data = json.load(f)
 
 for i in data['_via_img_metadata']:
     for j in data['_via_img_metadata'][i]['regions']:
-        print (j['shape_attributes']['all_points_x'])
-        print (j['shape_attributes']['all_points_y'])
+        print (j['shape_attributes']['all_points_x'],j['shape_attributes']['all_points_y'])
+
 f.close()
 
 #with open('via_data_formatted.txt',"w") as write_file:
